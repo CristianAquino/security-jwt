88242c1a9f048847a259adaff476377da43d2399
cristianaquino <ycristian10@gmail.com>
2026-02-01 17:30:31 -0500

    feat: auth service

--- src/main/java/security/security_jwt/Auth/AuthService.java
+++ src/main/java/security/security_jwt/Auth/AuthService.java
@@ -1,30 +1,42 @@
 package security.security_jwt.Auth;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import security.security_jwt.Auth.Models.AuthResponse;
 import security.security_jwt.Auth.Models.LoginRequest;
 import security.security_jwt.Auth.Models.RegisterRequest;
+import security.security_jwt.Jwt.JwtService;
 import security.security_jwt.User.Role;
 import security.security_jwt.User.User;
 import security.security_jwt.User.UserRepository;
 
-import java.net.URI;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     public AuthResponse login(LoginRequest req) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(req.getUsername(),req.getPassword()));
+        UserDetails user = userRepository.findByUsername(req.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse
+                .builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponse register(RegisterRequest req) {
         User user = User.builder()
                 .username(req.getUsername())
-                .password(req.getPassword())
+                .password(passwordEncoder.encode(req.getPassword()))
                 .firstname(req.getFirstname())
                 .lastname(req.getLastname())
                 .country(req.getCountry())
@@ -34,7 +46,7 @@ public class AuthService {
 
         return AuthResponse
                 .builder()
-                .token(null)
+                .token(jwtService.getToken(user))
                 .build();
     }
 }
